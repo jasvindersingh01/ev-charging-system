d39648b17130bf4abb34c0c1ed430b082dc5dadc
Jasvinder Singh <heyjasvinder@gmail.com>
2026-04-18 01:00:44 +0530

    Update and live

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://voltify-ev.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/stations", stationRoutes);
